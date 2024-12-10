793da9ecd99a32787c56f8c89c2c869564575a92
sebas-777 <sebastiancast06@hotmail.com>
2024-12-10 12:37:50 -0500

    [sc] initial commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,8 @@
+package src;
+
+public class HolaMundo {
+    //Agregamos el metodo principal
+    public static void main(String[] args) {
+        System.out.println("Hola pendejos! estoy aprendiendo java ");
+    }
+}
